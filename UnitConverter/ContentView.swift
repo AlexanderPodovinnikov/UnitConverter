1b40849d2bc4b0cc89b45728e88b23214482e5b1
Alex Po <for.sol@gmail.com>
2022-05-13 16:55:01 +0300

    Initial Commit

---
+++ UnitConverter/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  UnitConverter
+//
+//  Created by Alex Po on 13.05.2022.
+//
+
+import SwiftUI
+
+enum Measure {
+    
+}
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
