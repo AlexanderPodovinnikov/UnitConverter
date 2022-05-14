63dbe3ad1480e31be8cc76dc6f9ba7cac3b6598c
Alex Po <for.sol@gmail.com>
2022-05-14 11:59:09 +0300

    Work well with length units - simple solution

--- UnitConverter/ContentView.swift
+++ UnitConverter/ContentView.swift
@@ -7,19 +7,112 @@
 
 import SwiftUI
 
-enum Measure {
-    
+enum Measure: String, CaseIterable {
+    case meeters
+    case kilomeeters
+    case feet
+    case yards
+    case miles
+    //var id: String {self.rawValue}
 }
 
 struct ContentView: View {
+
+    @State private var inputValue: Double = 1
+    @State private var inputUnit = Measure.meeters
+    @State private var outputUnit =  Measure.yards
+    
+    private var outputValue: Double {
+        convert(lengh: inputValue, from: inputUnit, to: outputUnit)
+    }
+    
+    let formatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 5
+        return formatter
+    }()
+    
+    @FocusState private var inputIsFocused: Bool
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section("Enter original value and unit") {
+                    TextField("Input value", value: $inputValue, format: .number)
+                        .keyboardType(.decimalPad)
+                        .focused($inputIsFocused)
+                    
+                    Picker("Select: ", selection: $inputUnit) {
+                        ForEach(Measure.allCases, id:\.self) {unit in
+                            
+                            Text(unit.rawValue)
+                        }
+                    
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                    Section {
+                        Picker("Select: ", selection: $outputUnit) {
+                            ForEach(Measure.allCases, id:\.self) {unit in
+                                
+                                Text(unit.rawValue)
+                            }
+                        }
+                        .pickerStyle(SegmentedPickerStyle())
+                        Text("\(NSNumber(value: outputValue), formatter: formatter)")
+                    } header: {
+                        Text("And choose unit to convert")
+                    }
+            }
+            .navigationTitle("Lenght converter")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    Button("Done") {
+                        inputIsFocused = false
+                    }
+                }
+            }
+        }
+    }
+    func calculate(lenght baseValue: Double, in unit: Measure) -> Double {
+        switch unit {
+        case .meeters:
+            return baseValue
+        case .kilomeeters:
+            return baseValue / 1000
+        case .feet:
+            return baseValue * 3.28084
+        case .yards:
+            return baseValue * 1.09361
+        case .miles:
+            return baseValue / 1609.34
+        }
+    }
+    
+    func convert(lengh input: Double, from: Measure, to unit: Measure) -> Double {
+        let baseValue: Double
+        switch from {
+        case .meeters:
+            baseValue = input
+        case .kilomeeters:
+            baseValue = input * 1000
+        case .feet:
+            baseValue = input / 3.28084
+        case .yards:
+            baseValue = input / 1.09361
+        case .miles:
+            baseValue = input * 1609.34
+        }
+        return calculate(lenght: baseValue, in: unit)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewInterfaceOrientation(.portrait)
     }
 }
